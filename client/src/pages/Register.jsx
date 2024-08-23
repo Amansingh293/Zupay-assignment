7f70ecb4044de51c7d464b10961769c208a836af
Amansingh293 <amansinghdl9c@gmail.com>
2024-08-23 13:46:13 +0530

    added loader in register page

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,14 +1,17 @@
 import { Button, Form, Input, message } from "antd";
 import axios from "axios";
-import React from "react";
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { base_url } from "../constants";
+import Loader from "../components/Loader";
 
 const Register = () => {
   const [form] = Form.useForm();
   const navigate = useNavigate();
+  const [loading, setLoading] = useState(false);
 
   const RegisterFuntion = async (values) => {
+    setLoading(true);
     try {
       await form.validateFields();
       const response = await axios.post(
@@ -18,6 +21,7 @@ const Register = () => {
 
       if (response.data.success) {
         message.success("Registered successfully!!");
+        setLoading(false);
         navigate("/login");
       } else {
         message.error("Something went wrong !! Login again !!");
@@ -25,67 +29,74 @@ const Register = () => {
     } catch (error) {
       console.error("Login failed:", error);
     }
+    setLoading(false);
   };
 
   return (
-    <div className="flex justify-center items-center h-[100%] w-full">
-      <Form
-        form={form}
-        name="Register"
-        onFinish={RegisterFuntion}
-        className="border rounded-xl shadow-xl p-7"
-      >
-        <Form.Item>
-          <h1 className="font-bold text-[1.1rem] text-center">
-            Enter Details to register!!
-          </h1>
-        </Form.Item>
+    <>
+      {loading ? (
+        <Loader />
+      ) : (
+        <div className="flex justify-center items-center h-[100%] w-full">
+          <Form
+            form={form}
+            name="Register"
+            onFinish={RegisterFuntion}
+            className="border rounded-xl shadow-xl p-7"
+          >
+            <Form.Item>
+              <h1 className="font-bold text-[1.1rem] text-center">
+                Enter Details to register!!
+              </h1>
+            </Form.Item>
 
-        <Form.Item
-          label="Username"
-          name="username"
-          rules={[
-            {
-              required: true,
-              message: "Please input your Username!",
-            },
-          ]}
-        >
-          <Input />
-        </Form.Item>
-        <Form.Item
-          label="Email"
-          name="email"
-          rules={[
-            {
-              required: true,
-              message: "Please input your Email!",
-            },
-          ]}
-        >
-          <Input />
-        </Form.Item>
+            <Form.Item
+              label="Username"
+              name="username"
+              rules={[
+                {
+                  required: true,
+                  message: "Please input your Username!",
+                },
+              ]}
+            >
+              <Input />
+            </Form.Item>
+            <Form.Item
+              label="Email"
+              name="email"
+              rules={[
+                {
+                  required: true,
+                  message: "Please input your Email!",
+                },
+              ]}
+            >
+              <Input />
+            </Form.Item>
 
-        <Form.Item
-          label="Password"
-          name="password"
-          rules={[
-            {
-              required: true,
-              message: "Please input your password!",
-            },
-          ]}
-        >
-          <Input.Password />
-        </Form.Item>
+            <Form.Item
+              label="Password"
+              name="password"
+              rules={[
+                {
+                  required: true,
+                  message: "Please input your password!",
+                },
+              ]}
+            >
+              <Input.Password />
+            </Form.Item>
 
-        <Form.Item>
-          <Button type="primary" htmlType="submit">
-            Submit
-          </Button>
-        </Form.Item>
-      </Form>
-    </div>
+            <Form.Item>
+              <Button type="primary" htmlType="submit">
+                Submit
+              </Button>
+            </Form.Item>
+          </Form>
+        </div>
+      )}
+    </>
   );
 };
 
